5f15e67ff520e66a6c3a283826a78943ca01bba8
Brian Inboden <binboden@blizzard.com>
2017-12-13 17:41:00 -0800

    added sleep command

--- AutoitNETtest/Program.cs
+++ AutoitNETtest/Program.cs
@@ -10,6 +10,7 @@ namespace AutoitNETtest
             AutoItX.Run("notepad.exe", "C:\\Windows\\System32");
             AutoItX.WinWaitActive("Untitled");
             AutoItX.Send("I'm in notepad");
+            AutoItX.Sleep(3000);
             IntPtr winHandle = AutoItX.WinGetHandle("Untitled");
             AutoItX.WinKill(winHandle);
         }
